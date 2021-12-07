0eed594f2e80971039dd80d39279ce087f39f2f6
Yuriy Kiselev <y.kiselev@SD-1690.local>
2021-12-07 17:41:54 +0200

    change get signatures to getConfirmedSignaturesForAddress2 method

--- src/api/signatures.ts
+++ src/api/signatures.ts
@@ -10,7 +10,7 @@ export const getSignatures = async (
 ): Promise<string[]> => {
   const params = [account, { limit: 10, before: before ? before : undefined }];
   const result = await request<TSignaturesResponse>(
-    'getSignaturesForAddress',
+    'getConfirmedSignaturesForAddress2',
     params
   );
   return result.map((s) => s.signature);
